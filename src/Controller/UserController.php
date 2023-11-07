d17d7bd2262ae3803c899e15a3be2b0650ba98ab
D0000ds <sharckpedgaming@gmail.com>
2023-11-07 16:49:45 +0100

    progress

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -3,21 +3,25 @@
 namespace App\Controller;
 
 use App\Entity\User;
+use App\Entity\Adresse;
 use App\Entity\Commande;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class UserController extends AbstractController
 {
     #[Route('/profile', name: 'app_user')]
-    public function index(EntityManagerInterface $entityManager): Response
+    public function index(EntityManagerInterface $entityManager,SessionInterface $session,): Response
     {
-        $commandes = $entityManager->getRepository(Commande::class)->findAll();
+        $clients = $entityManager->getRepository(User::class)->findAll();
+        $commande = $entityManager->getRepository(Commande::class)->findBy(['client' => $this->getUser()->getId()]);
 
         return $this->render('user/index.html.twig', [
-            'commandes' => $commandes,
+            'clients' => $clients,
+            'commande' => $commande,
         ]);
     }
 }
